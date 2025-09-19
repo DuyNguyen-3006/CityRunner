0536a9153d683f3f260920d6fcea00329387f157
tucnzz112 <vohuynhtuandan@gmail.com>
2025-09-19 23:13:17 +0700

    feat/Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine.Rendering;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private float jumpAnimDuration = 0.35f;
 
     private Rigidbody2D rb;
     private SortingGroup sortingGroup;
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
         rb.freezeRotation = true;
 
         baseScale = transform.localScale;
+
         anim.SetBool("isRunning", false);
         anim.SetBool("isJumping", false);
     }
@@ -35,9 +36,7 @@ public class PlayerController : MonoBehaviour
         {
             isJumping = true;
             anim.SetBool("isJumping", true);
-
-
-            Invoke(nameof(EndJump), 0.35f);
+            Invoke(nameof(EndJump), jumpAnimDuration);
         }
     }
 
@@ -49,17 +48,12 @@ public class PlayerController : MonoBehaviour
         );
 
         rb.linearVelocity = input.normalized * moveSpeed;
-
-
         if (input.x < 0f)
             transform.localScale = new Vector3(-Mathf.Abs(baseScale.x), baseScale.y, baseScale.z);
         else if (input.x > 0f)
             transform.localScale = new Vector3(+Mathf.Abs(baseScale.x), baseScale.y, baseScale.z);
-
-
         anim.SetBool("isRunning", rb.linearVelocity.sqrMagnitude > 0.01f);
     }
-
     void EndJump()
     {
         anim.SetBool("isJumping", false);
