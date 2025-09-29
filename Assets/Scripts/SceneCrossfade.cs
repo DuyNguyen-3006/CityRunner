3434ea6ae968f8c25e4696fb8f966356e1192d22
Minhtamkim <kimngocminhtam.97.1819@gmail.com>
2025-09-29 09:46:32 +0700

    config intro

---
+++ Assets/Scripts/SceneCrossfade.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneCrossfade : MonoBehaviour
+{
+    public static SceneCrossfade Instance { get; private set; }
+
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1.5f;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        SetAlpha(0); // Đảm bảo ban đầu trong suốt
+    }
+
+    public void CrossfadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeAndLoad(sceneName));
+    }
+
+    private IEnumerator FadeAndLoad(string sceneName)
+    {
+        yield return StartCoroutine(Fade(0, 1));
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator Fade(float from, float to)
+    {
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / fadeDuration);
+            SetAlpha(Mathf.Lerp(from, to, t));
+            yield return null;
+        }
+        SetAlpha(to);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        if (fadeImage != null)
+        {
+            Color c = fadeImage.color;
+            c.a = alpha;
+            fadeImage.color = c;
+        }
+    }
+}
